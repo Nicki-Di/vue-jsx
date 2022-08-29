eadf036058377a871d2a24c80859511b54f20070
nicky <fatemesahraee2000@gmail.com>
2022-08-29 12:07:25 +0430

    Revert "initial"
    This reverts commit e6536b8dab11c5311fb2844861bfea9e9f788fe5.

---
+++ src/components/CoinCards.vue
@@ -0,0 +1,18 @@
+<script setup>
+import SingleCoinCard from "@/components/SingleCoinCard.vue";
+
+const cards = [
+  {type: "red", image: "1", amount: 24009874, percentage: 44},
+  {type: "green", image: "2", amount: 14009874, percentage: 32},
+  {type: "red", image: "3", amount: 2445239874, percentage: 12},
+  {type: "green", image: "4", amount: 7649874, percentage: 9},
+]
+</script>
+
+<template>
+  <div class = "flex flex-row gap-8 mt-8 ">
+    <SingleCoinCard v-for = "card in cards" :data = "card"/>
+
+  </div>
+
+</template>
---
+++ src/components/IconBadge.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps(['icon', 'number'])
+
+</script>
+
+<template>
+  <div class="relative bg-primary text-s-100 h-10 w-10 p-2 rounded-lg ">
+    <span class="absolute -top-1 -right-1 inline-flex justify-center items-center w-5 h-5 b2 bg-accent  rounded-full">
+    {{number}}
+  </span>
+    <component :is = "icon" aria-hidden = "true"/>
+
+  </div>
+
+</template>
